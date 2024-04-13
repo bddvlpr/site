799cbd2c025f5902caef0fe9b0b7aa5599ec3056
Luna Simons <luna@bddvlpr.com>
2024-04-13 19:21:20 +0200

    chore: init project files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,17 @@
+/** @type { import("eslint").Linter.Config } */
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
 	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'plugin:perfectionist/recommended-natural',
+		'prettier'
+	],
 	overrides: [
 		{
 			files: ['*.svelte'],
@@ -26,5 +20,13 @@ module.exports = {
 				parser: '@typescript-eslint/parser'
 			}
 		}
-	]
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte'],
+		sourceType: 'module'
+	},
+	plugins: ['@typescript-eslint'],
+	root: true
 };
--- src/app.d.ts
+++ src/app.d.ts
@@ -2,8 +2,4 @@ declare global {
 	namespace App {}
 }
 
-declare module '@fortawesome/free-brands-svg-icon/index.es' {
-	export * from '@fortawesome/free-brands-svg-icons';
-}
-
 export {};
