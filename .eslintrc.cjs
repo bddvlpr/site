8888b51f62ddceeccc7b3d4237c4c569d4f23a22
Luna Simons <luna@bddvlpr.com>
2024-04-14 18:27:28 +0200

    style: use 2 space tab width

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,32 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'plugin:perfectionist/recommended-natural',
-		'prettier'
-	],
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte'],
-		sourceType: 'module'
-	},
-	plugins: ['@typescript-eslint'],
-	root: true
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'plugin:perfectionist/recommended-natural',
+    'prettier'
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte'],
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  root: true
 };
