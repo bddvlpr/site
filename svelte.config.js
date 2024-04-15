67dfd3c3d3332850cbbfc1ec310f1869f19a02e9
Luna Simons <luna@bddvlpr.com>
2024-04-15 20:10:24 +0200

    feat: remark-toc and rehype-slug

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
+import rehypeSlug from 'rehype-slug';
+import remarkToc from 'remark-toc';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +13,9 @@ const config = {
   preprocess: [
     vitePreprocess(),
     mdsvex({
-      extensions: ['.svx']
+      extensions: ['.svx'],
+      rehypePlugins: [rehypeSlug],
+      remarkPlugins: [[remarkToc, { tight: true }]]
     })
   ]
 };
